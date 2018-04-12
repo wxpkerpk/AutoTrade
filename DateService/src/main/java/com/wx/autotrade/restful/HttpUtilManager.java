8e7f3f3d1d24ba52964c7fa6cd2c89c2b177fbd1
wx <wxpker@gmail.com>
2018-04-12 17:56:57 +0800

    mybaits supported

--- DateService/src/main/java/com/wx/autotrade/restful/HttpUtilManager.java
+++ DateService/src/main/java/com/wx/autotrade/restful/HttpUtilManager.java
@@ -2,6 +2,10 @@ package com.wx.autotrade.restful;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -9,18 +13,20 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.http.Consts;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpException;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
+import org.apache.http.*;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.ConnectTimeoutException;
 import org.apache.http.conn.ConnectionKeepAliveStrategy;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
@@ -35,7 +41,9 @@ import org.apache.http.protocol.HttpContext;
  * @author zhangchi
  *
  */
+
 public class HttpUtilManager {
+
 
 	private static HttpUtilManager instance = new HttpUtilManager();
 	private static HttpClient client;
@@ -55,8 +63,58 @@ public class HttpUtilManager {
 	    }  
 	  
 	};
+	static class MyConnectionSocketFactory implements ConnectionSocketFactory {
+
+		@Override
+		public Socket createSocket(final HttpContext context) throws IOException {
+			InetSocketAddress socksaddr = (InetSocketAddress) context.getAttribute("socks.address");
+			// socket代理
+			Proxy proxy = new Proxy(Proxy.Type.SOCKS, socksaddr);
+			return new Socket(proxy);
+		}
+
+		@Override
+		public Socket connectSocket(
+				final int connectTimeout,
+				final Socket socket,
+				final HttpHost host,
+				final InetSocketAddress remoteAddress,
+				final InetSocketAddress localAddress,
+				final HttpContext context) throws IOException, ConnectTimeoutException {
+			Socket sock;
+			if (socket != null) {
+				sock = socket;
+			} else {
+				sock = createSocket(context);
+			}
+			if (localAddress != null) {
+				sock.bind(localAddress);
+			}
+			try {
+				sock.connect(remoteAddress, connectTimeout);
+			} catch (SocketTimeoutException ex) {
+				throw new ConnectTimeoutException(ex, host, remoteAddress.getAddress());
+			}
+			return sock;
+		}
+
+	}
 	private HttpUtilManager() {
-		client = HttpClients.custom().setConnectionManager(cm).setKeepAliveStrategy(keepAliveStrat).build(); 
+		Registry<ConnectionSocketFactory> reg = RegistryBuilder.<ConnectionSocketFactory> create()
+				.register("http", new MyConnectionSocketFactory()).build();
+		// HTTP客户端连接管理池
+		PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager(reg);
+		CloseableHttpClient httpclient = HttpClients.custom()
+				.setConnectionManager(connManager)
+		.setKeepAliveStrategy(keepAliveStrat)
+				.build();
+		InetSocketAddress socksaddr = new InetSocketAddress("127.0.0.1", 1234);
+		HttpClientContext context = HttpClientContext.create();
+		context.setAttribute("socks.address", socksaddr);
+		//把代理设置到请求配置
+		RequestConfig defaultRequestConfig = RequestConfig.custom()
+				.build();
+		client = HttpClients.custom().setConnectionManager(cm).setKeepAliveStrategy(keepAliveStrat).build();
 	}
 
     public static void IdleConnectionMonitor(){
