7a3aaad6b64905b18d758e72eb51daf7a19746fb
wx <wxpker@gmail.com>
2018-04-12 17:45:44 +0800

    mybaits supported

---
+++ DateService/src/main/java/com/wx/autotrade/start/SpringUtils.java
@@ -0,0 +1,63 @@
+package com.wx.autotrade.start;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+public class SpringUtils implements ApplicationListener<ContextRefreshedEvent> {
+
+    /**
+     * 上下文对象实例
+     */
+    private static ApplicationContext applicationContext;
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        System.out.println("应用启动完成");
+        this.applicationContext = contextRefreshedEvent.getApplicationContext();
+    }
+
+    /**
+     * 获取applicationContext
+     *
+     * @return
+     */
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    /**
+     * 通过name获取 Bean.
+     *
+     * @param name
+     * @return
+     */
+    public static Object getBean(String name) {
+        return getApplicationContext().getBean(name);
+    }
+
+    /**
+     * 通过class获取Bean.
+     *
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(Class<T> clazz) {
+        return getApplicationContext().getBean(clazz);
+    }
+
+    /**
+     * 通过name,以及Clazz返回指定的Bean
+     *
+     * @param name
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(String name, Class<T> clazz) {
+        return getApplicationContext().getBean(name, clazz);
+    }
+}
