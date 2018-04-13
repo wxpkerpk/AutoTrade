7db2672525cc822fdc8e3d89b4f5005b2c65f23b
wx <wxpker@gmail.com>
2018-04-13 17:46:04 +0800

    mybaits supported

--- DateService/src/main/java/com/wx/autotrade/mapper/PriceMapper.java
+++ DateService/src/main/java/com/wx/autotrade/mapper/PriceMapper.java
@@ -8,6 +8,6 @@ import java.util.List;
 public interface PriceMapper {
     @Select("SELECT * FROM price WHERE name = #{name} and date >= #{time}")
     List<Price> getByNameAndTime(@Param(value = "name") String name,@Param(value = "time")Date time);
-    @Insert("INSERT INTO price(name,date,maxPrice,minPrice,avgPrice,depth,intervals,openPrice,closePrice,id,val) VALUES(#{name}, #{date}, #{maxPrice}, #{minPrice}, #{avgPrice}, #{depth}, #{intervals}, #{openPrice}, #{closePrice},#{id},#{vol})")
+    @Insert("INSERT INTO price(name,date,maxPrice,minPrice,avgPrice,depth,intervals,openPrice,closePrice,id,vol) VALUES(#{name}, #{date}, #{maxPrice}, #{minPrice}, #{avgPrice}, #{depth}, #{intervals}, #{openPrice}, #{closePrice},#{id},#{vol})")
     void insert(Price price);
 }
--- DateService/src/main/scala/com/wx/autotrade/service/DataCollectService.scala
+++ DateService/src/main/scala/com/wx/autotrade/service/DataCollectService.scala
@@ -17,8 +17,9 @@ import org.json4s.jackson.JsonMethods._
 
 import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
+import scala.concurrent.Future
 
-case class Ticker(buy: Float, sell: Float, last: Float, vol: Double) {
+case class Ticker(buy: Double, sell: Double, last: Double, vol: Double) {
   override def equals(obj: scala.Any): Boolean = {
     obj match {
       case otherTicker: Ticker => buy == otherTicker.buy && sell == otherTicker.sell && last == otherTicker.last && vol == otherTicker.vol
@@ -49,19 +50,19 @@ class DataCollectService extends Actor {
     case _ => ()
   }
 
-  def caculatePrice(name:String,arrayBuffer: ArrayBuffer[Ticker])={
+  def caculatePrice(name:String,arrayBuffer: Array[Ticker])={
     val priceArray=arrayBuffer.map(_.last)
     val max=priceArray.max
     val min=priceArray.min
     val avg=priceArray.sum/priceArray.length
     val begin=priceArray.head
-    val end=priceArray.takeRight(0).head
+    val end=priceArray(priceArray.length-1)
+    val price=new Price(UUID.randomUUID().toString,name,new Date(),max.toFloat,min.toFloat,avg.toFloat,
+      begin.toFloat,end.toFloat,"",
+      (intervals._1*priceArray.length).toInt)
 
-    val interval=(queueMaxLength*intervals)._1.toInt
 
-    val price=new Price(UUID.randomUUID().toString,name,new Date(),max,min,avg,begin,end,"",1)
-
-
+    price.setVol(arrayBuffer(arrayBuffer.length-1).vol.toInt)
     price
   }
   def makeData(mapper: PriceMapper, clientService: StockClientService) = {
@@ -71,26 +72,32 @@ class DataCollectService extends Actor {
       var success=false
       try {
         val str=client.ticker(name)
-        val tickerJson = parse(str)
-        val ticker = Ticker((tickerJson \ "buy").extract[Float],
-          (tickerJson \ "sell").extract[Float],
-          (tickerJson \ "last").extract[Float],
-          (tickerJson \ "vol").extract[Double])
-        coinsTickers.getOrElseUpdate("name", {
+        val tickerJson = parse(str) \ "ticker"
+        val ticker = Ticker((tickerJson \ "buy").extract[String].toFloat,
+          (tickerJson \ "sell").extract[String].toFloat,
+          (tickerJson \ "last").extract[String].toFloat,
+          (tickerJson \ "vol").extract[String].toFloat)
+        coinsTickers.getOrElseUpdate(name, {
           new ArrayBuffer[Ticker]()
         }) += ticker
         success=true
 
       } catch {
-        case e:Throwable  => println(s"${name}获取数据发生异常${e.toString}")
+        case e:Throwable  => println(s"${name}获取数据发生异常: ${e.toString}")
 
       }
-      if (success&&coinsTickers("name").length>10) {
-        val price=caculatePrice(name,coinsTickers("name"))
-        val depth=client.depth(name)
-        price.setDepth(depth)
+      if (success&&coinsTickers(name).length>=10) {
+        val price=caculatePrice(name,coinsTickers(name).toArray)
+        coinsTickers(name).clear()
+
+        val depth=parse(client.depth(name))
+        val ask=(depth \ "asks").extract[Array[Array[Float]]].map(_(1)).sum
+        val bids=(depth \ "bids").extract[Array[Array[Float]]].map(_(1)).sum
+        val depthStatics=s"{ask:$ask,bids:$bids}"
+
+        price.setDepth(depthStatics)
         mapper.insert(price)
-        coinsTickers("name").clear()
+
       }
 
     })
@@ -102,7 +109,7 @@ object DataCollectService {
   val coinsType = new mutable.LinkedHashSet[String]
 
   val intervals=3 seconds
-  val queueMaxLength=10
+  val queueMaxLength=20
   val system = ActorSystem("autoTrade")
   val act1 = system.actorOf(Props[DataCollectService], "autoTrade")
   implicit val time = Timeout(5 seconds)
@@ -114,7 +121,7 @@ object DataCollectService {
 
     import scala.concurrent.ExecutionContext.Implicits.global
     val tick = "tick"
-    val cancellable = system.scheduler.schedule( 10 seconds,intervals, act1, tick)
+    val cancellable = system.scheduler.schedule( 2 seconds,intervals, act1, tick)
 
   }
 
